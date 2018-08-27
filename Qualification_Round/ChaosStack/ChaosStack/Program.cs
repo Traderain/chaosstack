7b6c867bb6e42dd800cb9d568d144b9b7ee36515
Ákos Köte <kote2ster@gmail.com>
2018-08-27 15:33:02 +0200

    Add Send_GetRequest/Send_PostRequest functions

--- Qualification_Round/ChaosStack/ChaosStack/Program.cs
+++ Qualification_Round/ChaosStack/ChaosStack/Program.cs
@@ -14,23 +14,49 @@ namespace ChaosStack
         private const string api_key = "6ZC9jyeffYUudGnrUNSRoijAL7R2";
         private const string base_url = " https://us-central1-chaosstack.cloudfunctions.net/";
 
-        private const string url_part = "/generate";
-        
-        [STAThread]
-        static void Main(string[] args)
+        static string Send_GetRequest(string uri)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(base_url+url_part);
-            request.Headers.Add("Authorization",api_key);
-            request.AutomaticDecompression = DecompressionMethods.GZip;
-            string json;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Headers.Add("Authorization", api_key);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
             using (Stream stream = response.GetResponseStream())
             using (StreamReader reader = new StreamReader(stream))
             {
-                json = reader.ReadToEnd();
+                return reader.ReadToEnd();
             }
-            Console.WriteLine(json);
-            Clipboard.SetText(json);
+        }
+
+        public string Send_PostRequest(string uri, string data, string contentType, string method = "POST")
+        {
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.ContentLength = dataBytes.Length;
+            request.ContentType = contentType;
+            request.Method = method;
+
+            using (Stream requestBody = request.GetRequestStream())
+            {
+                requestBody.Write(dataBytes, 0, dataBytes.Length);
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var genResponse = Send_GetRequest(base_url + "/generate");
+            Console.WriteLine(genResponse);
+            Clipboard.SetText(genResponse);
             Console.ReadLine();
         }
     }
